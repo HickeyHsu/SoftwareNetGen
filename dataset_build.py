612e5ce229adeb3e5bba6227fb952262a08e1000
HickeyHsu <alex454141@163.com>
2022-06-07 09:20:51 +0800

    Update dataset_build.py

--- dataset_build.py
+++ dataset_build.py
@@ -26,16 +26,21 @@ class DatasetBuilder:
         csv_data=pd.read_csv(csv_file)
         csv_data['abs_path']=csv_data["File"].apply(lambda x:os.path.join(source_directory,x))
         print(csv_data)
-    def merge(self,version):
+    def mergeBug(self,version):
         source_directory=os.path.join(self.data_directory,"src",self.project_name,self.project_name+"-"+version)
         output_dir=os.path.join(self.data_directory,"metric",self.project_name,self.project_name+"-"+version)
+        
+        local_metric_df=pd.read_csv(os.path.join(output_dir,"emerge","metric.csv"))
+        local_metric_df=local_metric_df[~local_metric_df["sna-abs-path"].isna()].set_index("sna-abs-path")
 
-        local_metric_df=pd.read_csv(os.path.join(output_dir,"emerge","metric.csv")).set_index("sna-abs-path")
         csv_file=os.path.join(self.data_directory,"csv",self.project_name,self.project_name+"-"+version+".csv")
         csv_data=pd.read_csv(csv_file)
-        csv_data['abs_path']=csv_data["File"].apply(lambda x:os.path.join(source_directory,x))
+        csv_data['abs_path']=csv_data["File"].apply(lambda x:os.path.join(source_directory,x).replace("/","\\"))
         csv_data=csv_data.set_index('abs_path')
+
         local_metric_df['RealBugCount']=csv_data['RealBugCount']
+        local_metric_df.to_csv(os.path.join(output_dir,"emerge","merged.csv"))
+        print(len(local_metric_df))
 if __name__ == '__main__':
     data_directory=r"D:\data_sci\dataset_jira"
     project_name="activemq"
@@ -43,5 +48,7 @@ if __name__ == '__main__':
     language="java"
     datasetBuilder =DatasetBuilder(data_directory,project_name,language)
     # datasetBuilder.start(version)
+    datasetBuilder.mergeBug(version)
+
 
 
